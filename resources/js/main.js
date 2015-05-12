f953e750050cc6dddd53a1fcf5e6ab6ac8a10c87
Trevor Blades <tdblades@gmail.com>
2015-05-12 09:26:12 -0700

    Graph responds to window resize, refactored graph drawing, add legend

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,160 +1,205 @@
 
 // Now entering D3 land. Enjoy your stay!
 
-function renderGraph(data, colors, focus) {
+var Graph = {
 
-  data = data.sort(function(a, b) {
-    return b[focus] - a[focus];
-  });
+  _data: [],
+  _colors: [],
+  _focus: null,
 
-  var statsGraph = d3.select('#sd-stats-graph');
-  var $statsGraph = $(statsGraph[0]);
-
-  var statsGraphConfig = {
-    width: $statsGraph.width(),
-    height: $statsGraph.height(),
-    margin: {
-      top: parseInt($statsGraph.css('padding-top')),
-      right: parseInt($statsGraph.css('padding-right')),
-      bottom: parseInt($statsGraph.css('padding-bottom')),
-      left: parseInt($statsGraph.css('padding-left'))
+  setData: function(args) {
+    for (var key in args) {
+      if (args.hasOwnProperty(key)) {
+        this['_' + key] = args[key];
+      }
     }
-  };
-  var statsGraphScale = {
-    x: d3.scale.linear()
-      .domain([0, data.length])
-      .range([0, statsGraphConfig.width]),
-    y: d3.scale.linear()
-      .domain([0, d3.max(data, function(d) {
-        return d.points;
-      })])
-      .range([statsGraphConfig.height, 0])
-  };
-  var statsGraphAxis = {
-    x: d3.svg.axis().scale(statsGraphScale.x).orient('bottom').ticks(0),
-    y: d3.svg.axis().scale(statsGraphScale.y).orient('left')
-  };
+    return this;
+  },
+  render: function() {
 
-  statsGraph = statsGraph.append('svg')
-    .attr('width', statsGraphConfig.width + (statsGraphConfig.margin.right + statsGraphConfig.margin.left))
-    .attr('height', statsGraphConfig.height + (statsGraphConfig.margin.top + statsGraphConfig.margin.bottom))
-  .append('g')
-    .attr('transform', 'translate(' + statsGraphConfig.margin.left + ', ' + statsGraphConfig.margin.top + ')');
+    var statsGraph = d3.select('#sd-stats-graph');
+    var $statsGraph = $(statsGraph[0]);
 
-  // Add axes
-  var statsGraphAxes = statsGraph.append('g')
-    .attr('class', 'sd-stats-graph-axes');
-  statsGraphAxes.append('g')
-    .attr('class', 'sd-stats-graph-axis')
-    .attr('transform', 'translate(0, ' + statsGraphConfig.height + ')')
-    .call(statsGraphAxis.x)
-    .append('text')
-      .attr('class', 'sd-stats-graph-axis-label')
-      .attr('transform', 'translate(' + (statsGraphConfig.width / 2) + ', 24)')
-      .attr('text-anchor', 'middle')
-      .text('Players');
-  statsGraphAxes.append('g')
-    .attr('class', 'sd-stats-graph-axis')
-    .call(statsGraphAxis.y);
+    function createSubject(fill, filteredData, pointsFill) {
 
-  var statsGraphLine = d3.svg.line()
-    .x(function(d, i) {
-      return statsGraphScale.x(i);
-    })
-    .y(function(d) {
-      return statsGraphScale.y(d);
-    });
-  var statsGraphArea = d3.svg.area()
-    .x(function(d, i) {
-      return statsGraphScale.x(i);
-    })
-    .y0(statsGraphConfig.height)
-    .y1(function(d) {
-      return statsGraphScale.y(d);
-    });
+      var statsGraphSubject = statsGraph.append('g')
+        .attr('class', 'sd-stats-graph-subject');
+      statsGraphSubject.append('path')
+        .attr('class', 'sd-stats-graph-area')
+        .attr('fill', fill)
+        .attr('d', statsGraphArea(filteredData));
+      statsGraphSubject.append('path')
+        .attr('class', 'sd-stats-graph-line')
+        .attr('d', statsGraphLine(filteredData));
 
-  function createSubject(fill, filteredData, showPoints) {
+      if (pointsFill) {
 
-    var statsGraphSubject = statsGraph.append('g')
-      .attr('class', 'sd-stats-graph-subject');
-    statsGraphSubject.append('path')
-      .attr('class', 'sd-stats-graph-area')
-      .attr('fill', fill)
-      .attr('d', statsGraphArea(filteredData));
-    statsGraphSubject.append('path')
-      .attr('class', 'sd-stats-graph-line')
-      .attr('d', statsGraphLine(filteredData));
+        var statsGraphPoint = statsGraphSubject.append('g')
+          .attr('class', 'sd-stats-graph-points')
+          .selectAll('.sd-stats-graph-point')
+            .data(data.reverse())
+          .enter().append('g')
+            .attr('class', 'sd-stats-graph-point')
+            .attr('transform', function(d, i) {
+              return 'translate(' + statsGraphScale.x(data.length - 1 - i) + ', ' + statsGraphScale.y(d.goals) + ')';
+            });
 
-    if (showPoints) {
+        var statsGraphLabel = statsGraphPoint.append('g')
+          .attr('class', 'sd-stats-graph-label');
+        var statsGraphLabelTextSize = 10;
+        statsGraphLabel.append('path')
+          .attr('d', 'M0,0L24,0');
+        var statsGraphLabelText = statsGraphLabel.append('text')
+          .text(function(d) {
+            return d.name;
+          })
+          .attr('transform', 'translate(36, 4)')
+          .attr('font-size', statsGraphLabelTextSize);
+        statsGraphLabel.insert('rect', 'text')
+          .attr('width', function(d, i) {
+            return statsGraphLabelText[0][i].getBBox().width + 24;
+          })
+          .attr('height', statsGraphLabelTextSize + 10)
+          .attr('transform', 'translate(24, ' + ((statsGraphLabelTextSize + 10) / -2) +')');
 
-      var statsGraphPoint = statsGraphSubject.append('g')
-        .attr('class', 'sd-stats-graph-points')
-        .selectAll('.sd-stats-graph-point')
-          .data(data.reverse())
-        .enter().append('g')
-          .attr('class', 'sd-stats-graph-point')
-          .attr('transform', function(d, i) {
-            return 'translate(' + statsGraphScale.x(data.length - 1 - i) + ', ' + statsGraphScale.y(d.goals) + ')';
-          });
+        statsGraphPoint.append('circle')
+          .attr('r', 3)
+          .attr('fill', pointsFill);
+        statsGraphPoint.append('circle')
+          .attr('r', 3)
+          .attr('fill', '#fff');
+      }
+    }
+    function createSubjects(subjects) {
+      subjects.forEach(function(subject) {
+        createSubject(subject.fill, subject.data, subject.pointsFill);
+      });
+    }
 
-      var statsGraphLabel = statsGraphPoint.append('g')
-        .attr('class', 'sd-stats-graph-label');
-      var statsGraphLabelTextSize = 10;
-      statsGraphLabel.append('path')
-        .attr('d', 'M0,0L24,0');
-      var statsGraphLabelText = statsGraphLabel.append('text')
-        .text(function(d) {
-          return d.name;
+    function createLegend(subjects) {
+      var legendHeight = 24;
+      var statsGraphLegend = statsGraph.append('g')
+        .attr('class', 'sd-stats-graph-legend')
+        .attr('transform', 'translate(' + (statsGraphConfig.width - legendHeight) + ', 0)');
+      subjects.forEach(function(subject, index) {
+        var statsGraphLegendItem = statsGraphLegend.append('g')
+          .attr('transform', 'translate(0, ' + ((legendHeight + 16) * index) + ')');
+        statsGraphLegendItem.append('text')
+          .text(subject.label)
+          .attr('text-anchor', 'end')
+          .attr('transform', 'translate(-16, 17)');
+        statsGraphLegendItem.append('rect')
+          .attr('width', legendHeight)
+          .attr('height', legendHeight)
+          .attr('fill', subject.fill);
+      });
+    }
+
+    if (this._data.length) {
+
+      $statsGraph.empty();
+
+      var data = this._data.sort(function(a, b) {
+        return b[this._focus] - a[this._focus];
+      }.bind(this));
+
+      var statsGraphConfig = {
+        width: $statsGraph.width(),
+        height: $statsGraph.height(),
+        margin: {
+          top: parseInt($statsGraph.css('padding-top')),
+          right: parseInt($statsGraph.css('padding-right')),
+          bottom: parseInt($statsGraph.css('padding-bottom')),
+          left: parseInt($statsGraph.css('padding-left'))
+        }
+      };
+      var statsGraphScale = {
+        x: d3.scale.linear()
+          .domain([0, data.length])
+          .range([0, statsGraphConfig.width]),
+        y: d3.scale.linear()
+          .domain([0, d3.max(data, function(d) {
+            return d.points;
+          })])
+          .range([statsGraphConfig.height, 0])
+      };
+      var statsGraphAxis = {
+        x: d3.svg.axis().scale(statsGraphScale.x).orient('bottom').ticks(0),
+        y: d3.svg.axis().scale(statsGraphScale.y).orient('left')
+      };
+
+      statsGraph = statsGraph.append('svg')
+        .attr('width', statsGraphConfig.width + (statsGraphConfig.margin.right + statsGraphConfig.margin.left))
+        .attr('height', statsGraphConfig.height + (statsGraphConfig.margin.top + statsGraphConfig.margin.bottom))
+      .append('g')
+        .attr('transform', 'translate(' + statsGraphConfig.margin.left + ', ' + statsGraphConfig.margin.top + ')');
+
+      // Add axes
+      var statsGraphAxes = statsGraph.append('g')
+        .attr('class', 'sd-stats-graph-axes');
+      statsGraphAxes.append('g')
+        .attr('class', 'sd-stats-graph-axis')
+        .attr('transform', 'translate(0, ' + statsGraphConfig.height + ')')
+        .call(statsGraphAxis.x)
+        .append('text')
+          .attr('class', 'sd-stats-graph-axis-label')
+          .attr('transform', 'translate(' + (statsGraphConfig.width / 2) + ', 24)')
+          .attr('text-anchor', 'middle')
+          .text('Players');
+      statsGraphAxes.append('g')
+        .attr('class', 'sd-stats-graph-axis')
+        .call(statsGraphAxis.y);
+
+      var statsGraphLine = d3.svg.line()
+        .x(function(d, i) {
+          return statsGraphScale.x(i);
         })
-        .attr('transform', 'translate(36, 4)')
-        .attr('font-size', statsGraphLabelTextSize);
-      statsGraphLabel.insert('rect', 'text')
-        .attr('width', function(d, i) {
-          return statsGraphLabelText[0][i].getBBox().width + 24;
+        .y(function(d) {
+          return statsGraphScale.y(d);
+        });
+      var statsGraphArea = d3.svg.area()
+        .x(function(d, i) {
+          return statsGraphScale.x(i);
         })
-        .attr('height', statsGraphLabelTextSize + 10)
-        .attr('transform', 'translate(24, ' + ((statsGraphLabelTextSize + 10) / -2) +')');
+        .y0(statsGraphConfig.height)
+        .y1(function(d) {
+          return statsGraphScale.y(d);
+        });
 
-      statsGraphPoint.append('circle')
-        .attr('r', 3)
-        .attr('fill', colors[1]);
-      statsGraphPoint.append('circle')
-        .attr('r', 3)
-        .attr('fill', '#fff');
+      // Plot subjects
+      var dataSubjects = [
+        {
+          label: 'Points',
+          fill: '#fff',
+          data: data.map(function(d) {
+            return d.points;
+          })
+        },
+        {
+          label: 'Assists',
+          fill: 'url(#diagonal-lines)',
+          data: data.map(function(d) {
+            return d.assists;
+          })
+        },
+        {
+          label: 'Goals',
+          fill: 'url(#dots)',
+          data: data.map(function(d) {
+            return d.goals;
+          }),
+          pointsFill: this._colors[1]
+        }
+      ];
+      createSubjects(dataSubjects);
+      createLegend(dataSubjects);
     }
   }
-  function createSubjects(subjects) {
-    subjects.forEach(function(subject) {
-      createSubject(subject.fill, subject.data, subject.showPoints);
-    });
-  }
+};
 
-  // Plot subjects
-  var dataSubjects = [
-    {
-      fill: '#fff',
-      data: data.map(function(d) {
-        return d.points;
-      }),
-      showPoints: false
-    },
-    {
-      fill: 'url(#diagonal-lines)',
-      data: data.map(function(d) {
-        return d.assists;
-      }),
-      showPoints: false
-    },
-    {
-      fill: 'url(#dots)',
-      data: data.map(function(d) {
-        return d.goals;
-      }),
-      showPoints: true
-    }
-  ];
-  createSubjects(dataSubjects);
-}
+$(window).on('resize', function() {
+  Graph.render();
+});
 
 // Now leaving D3 land. Thanks for visiting!
 // 
@@ -167,7 +212,11 @@ var StatsView = Backbone.View.extend({
   template: _.template($('#template-sd-stats').html()),
   render: function() {
     this.$el.html(this.template(this.model.toJSON()));
-    renderGraph(this.model.get('players').toJSON(), this.model.get('colors'), 'goals');
+    Graph.setData({
+      data: this.model.get('players').toJSON(),
+      colors: this.model.get('colors'),
+      focus: 'goals'
+    }).render();
   }
 });
 
